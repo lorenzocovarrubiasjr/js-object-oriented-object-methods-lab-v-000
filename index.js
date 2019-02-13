2898c1501e133521a7eece3ac9dcabe5a0ae6ad1
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-13 02:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name, homeState, training) {
   this.training = training;
   this.veto = function() {
     return 'No, I must disagree'
-  } 
+  }
   this.approve = function() {
     return 'You can do that!'
   }
